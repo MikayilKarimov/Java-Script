715dd49a35a396aca7df48e5bacc0c96768bd21f
mikayil <Mikayl@DESKTOP-N5E06UH.mikayil.com>
2023-10-30 05:41:00 -0700

    ortamalar elave edildi

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-document.write("Hello World");
-alert("Yuklenmedi");
-console.log(window)
-blur("dasdas")
+// document.write("Hello World");
+// alert("Yuklenmedi");
+// console.log(window)
+
+// let isim = prompt("adini gir");
+// let yas = prompt("yasinizi giriniz");
+
+// console.log ("Kullanici adi : " + isim);
+// console.log("Kullanici yasi : " + yas);
+
+//  let deleted = confirm("delete");
+//  console.log(deleted);
+
+
+// let note = 65;
+// if(note >= 65){
+//     console.log("Puan durumunuz : " + note);
+
+// }else{console.log("Gecemediniz");
+// }
+
+
+// let yas = Number(prompt("Yasinizi giriniz"));
+// let para = Number(prompt("Yasinizi giriniz"));
+
+// if(yas >=18 && para >3000){
+//     alert("ehliyyet alabilirsin")
+// }else{
+//     alert("ehliyyet alamazsin")
+// }
+
+
